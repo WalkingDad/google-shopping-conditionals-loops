43cd33e5579cbec13a9e82ab1f0d139261f19fba
Kent Deger <kdeger12@hotmail.com>
2016-05-26 08:20:08 -0700

    New Homework File added

--- src/js/google-shopping.js
+++ src/js/google-shopping.js
@@ -1,5 +1,9 @@
+//console.log(data.items.kind["shopping#products"]);
 var data = require('../products.json');
+var counter = 0
 
-// Write your solutions below
-
-console.log(data.items);
+for (var i = 0; i < data.items.length; i++) {
+ console.log(data.items[i].product.brand)
+ console.log(data.items[i].product.inventories[0].price)
+ console.log(data.items[i].product.link)
+}
